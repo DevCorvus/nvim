534319e91745eeb3b0e7f20df22f0adb0b9ff92b
DevCorvus <devcorvus.contact@gmail.com>
2024-09-30 01:08:49 -0400

    feat(conform): add support for more prettier-related filetypes

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,10 +1,15 @@
 require("conform").setup({
     formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
         python = { "isort", "black" },
         go = { "goimports", "gofmt" },
         rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        vue = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = {
         -- These options will be passed to conform.format()
